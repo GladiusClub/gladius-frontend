59cb0242d7c4a952319954bf03f2a0f0150942ca
akg3939 <ankitgupta3939@gmail.com>
2024-02-20 09:17:42 +0530

    Router and tailwind setup

---
+++ src/constants/routes.js
@@ -0,0 +1,13 @@
+export const protectedRoutes = {
+  home: "/home",
+  calendar: "/calendar",
+  tasks: "/tasks",
+  leaderboard: "/leaderboard",
+  profile: "/profile",
+};
+
+export const unProtectedRoutes = {
+  signIn: "/signin",
+  signUp: "/signup",
+  welcome: "/welcome",
+};
