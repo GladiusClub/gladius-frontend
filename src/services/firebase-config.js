44be0086bb40eae0c052a31fd950a8d65664c391
akg3939 <ankitgupta3939@gmail.com>
2024-02-29 12:21:55 +0530

    Setup firebase and redirections

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
@@ -15,4 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-signOut(auth)
