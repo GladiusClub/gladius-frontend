6c2d0b79ba355cc083d4333ca76721ad2b990862
akg3939 <ankitgupta3939@gmail.com>
2024-05-26 13:51:34 +0530

    GLC fn implementation on home page

--- src/modules/home/ScoreOverview/index.js
+++ src/modules/home/ScoreOverview/index.js
@@ -1,55 +1,25 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useMemo } from "react";
-import dayjs from "dayjs";
+import React from "react";
 import { Box } from "@mui/material";
-import { PiCoins } from "react-icons/pi";
 import { TbJewishStarFilled } from "react-icons/tb";
 
 import Typography from "components/Typography";
-import useUserProfile from "context/userProfile/useUserProfile";
-import useAttendedEvents from "hooks/useAttendedEvents";
-import { getPointsAndPercentInWeek } from "modules/utils";
 import colors from "theme/colors";
 import league from "assets/league.svg";
 import diamond from "assets/diamond.svg";
+import GlcPoints from "./GlcPoints";
 
 const style = {
   boxShadow: `0px 0px 20px 0px ${colors.shadow}`,
 };
 
 const ScoreOverview = () => {
-  const { user } = useUserProfile();
-  const { attendedEvents, getAttendedEvents } = useAttendedEvents();
-
-  useEffect(() => {
-    if (user.club) {
-      getAttendedEvents({ maxDate: dayjs() });
-    }
-  }, [user.club]);
-
-  const pointsBalance = useMemo(() => {
-    return attendedEvents.data.reduce((acc, curr) => acc + curr.score, 0);
-  }, [attendedEvents.data]);
-
-  const { pointsInWeek } = useMemo(() => {
-    return getPointsAndPercentInWeek(pointsBalance, attendedEvents.data);
-  }, [attendedEvents.data, pointsBalance]);
-
   return (
     <Box className="flex justify-around gap-4 mt-5">
       <Box
         style={style}
         className="bg-gradient-dark p-3 rounded-2xl text-center w-1/2"
       >
-        <div className="flex items-center gap-2 justify-center">
-          <PiCoins className="text-primary w-7 h-7" />
-          <Typography variant="h2" className="text-secondary">
-            {pointsBalance.toLocaleString()}
-          </Typography>
-        </div>
-        <Typography className="text-neutral text-sm">
-          + {pointsInWeek} last week
-        </Typography>
+        <GlcPoints />
 
         <div className="flex items-center gap-2 mt-5 justify-center">
           <TbJewishStarFilled className="text-primary w-7 h-7" />
